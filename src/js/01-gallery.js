0e669a58beb3499e665d4915318faa99d994c9f2
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-04-16 00:41:31 +0300

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,9 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+console.log("hello peace")
+
+import SimpleLightbox from "simplelightbox";

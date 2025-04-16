e6d22c786e9a231874bc3f3d7be73f246ebec7a8
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-04-17 00:34:09 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,48 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-console.log("hello peace")
-
+// Описаний в документації
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery");
+
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      
+      <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>
+  `
+    )
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+
+galleryEl.addEventListener("click", onClickGallery);
+
+function onClickGallery(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  showImage();
+}
+
+function showImage(target) {
+  new SimpleLightbox(".gallery a", {
+    nav: true,
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    close: true,
+    download: "downloads",
+  });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+
+player.on('timeupdate', function () {
+  console.log('played the video!',);
+
+  // let currentTime = getCurrentTime();
+  console.log(currentTime.seconds)
+});
+
+
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
